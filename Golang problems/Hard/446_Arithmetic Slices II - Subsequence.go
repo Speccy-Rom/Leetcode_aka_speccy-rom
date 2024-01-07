f51dfa8d3e1f447ffc232f795c7d28e2c178cef8
speccy-rom <speccy.rom@gmail.com>
2024-01-07 21:36:50 +0300

    [#446] : Arithmetic Slices II - Subsequence

---
+++ Golang problems/Hard/446_Arithmetic Slices II - Subsequence.go
@@ -0,0 +1,63 @@
+/*
+
+Given an integer array nums, return the number of all the arithmetic subsequences of nums.
+
+A sequence of numbers is called arithmetic if it consists of at least three elements and if the difference between any two consecutive elements is the same.
+
+For example, [1, 3, 5, 7, 9], [7, 7, 7, 7], and [3, -1, -5, -9] are arithmetic sequences.
+For example, [1, 1, 2, 5, 7] is not an arithmetic sequence.
+A subsequence of an array is a sequence that can be formed by removing some elements (possibly none) of the array.
+
+For example, [2,5,10] is a subsequence of [1,2,1,2,4,1,5,10].
+The test cases are generated so that the answer fits in 32-bit integer.
+
+
+
+Example 1:
+
+Input: nums = [2,4,6,8,10]
+Output: 7
+Explanation: All arithmetic subsequence slices are:
+[2,4,6]
+[4,6,8]
+[6,8,10]
+[2,4,6,8]
+[4,6,8,10]
+[2,4,6,8,10]
+[2,6,10]
+Example 2:
+
+Input: nums = [7,7,7,7,7]
+Output: 16
+Explanation: Any subsequence of this array is arithmetic.
+
+
+Constraints:
+
+1  <= nums.length <= 1000
+-231 <= nums[i] <= 231 - 1
+
+*/
+
+package main
+
+func numberOfArithmeticSlices(nums []int) (ans int) {
+	f := make([]map[int]int, len(nums))
+	for i := range f {
+		f[i] = map[int]int{}
+	}
+	for i, x := range nums {
+		for j, y := range nums[:i] {
+			d := x - y
+			cnt := f[j][d]
+			ans += cnt
+			f[i][d] += cnt + 1
+		}
+	}
+	return
+}
+
+func main() {
+	println(numberOfArithmeticSlices([]int{2, 4, 6, 8, 10}))
+	println(numberOfArithmeticSlices([]int{7, 7, 7, 7, 7}))
+}
