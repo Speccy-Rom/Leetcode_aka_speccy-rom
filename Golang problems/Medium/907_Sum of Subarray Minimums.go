494709a8b090f411bd3263d0f2ec5b8d1711031e
speccy-rom <speccy.rom@gmail.com>
2024-01-20 21:08:31 +0300

    [#907] : Sum of Subarray Minimums

---
+++ Golang problems/Medium/907_Sum of Subarray Minimums.go
@@ -0,0 +1,67 @@
+/*
+Given an array of integers arr, find the sum of min(b), where b ranges over every (contiguous) subarray of arr. Since the answer may be large, return the answer modulo 109 + 7.
+
+
+
+Example 1:
+
+Input: arr = [3,1,2,4]
+Output: 17
+Explanation:
+Subarrays are [3], [1], [2], [4], [3,1], [1,2], [2,4], [3,1,2], [1,2,4], [3,1,2,4].
+Minimums are 3, 1, 2, 4, 1, 1, 2, 1, 1, 1.
+Sum is 17.
+Example 2:
+
+Input: arr = [11,81,94,43,3]
+Output: 444
+
+
+Constraints:
+
+1 <= arr.length <= 3 * 104
+1 <= arr[i] <= 3 * 104
+*/
+
+package main
+
+func sumSubarrayMins(arr []int) (ans int) {
+	n := len(arr)
+	left := make([]int, n)
+	right := make([]int, n)
+	for i := range left {
+		left[i] = -1
+		right[i] = n
+	}
+	stk := []int{}
+	for i, v := range arr {
+		for len(stk) > 0 && arr[stk[len(stk)-1]] >= v {
+			stk = stk[:len(stk)-1]
+		}
+		if len(stk) > 0 {
+			left[i] = stk[len(stk)-1]
+		}
+		stk = append(stk, i)
+	}
+	stk = []int{}
+	for i := n - 1; i >= 0; i-- {
+		for len(stk) > 0 && arr[stk[len(stk)-1]] > arr[i] {
+			stk = stk[:len(stk)-1]
+		}
+		if len(stk) > 0 {
+			right[i] = stk[len(stk)-1]
+		}
+		stk = append(stk, i)
+	}
+	const mod int = 1e9 + 7
+	for i, v := range arr {
+		ans += (i - left[i]) * (right[i] - i) * v % mod
+		ans %= mod
+	}
+	return
+}
+
+func main() {
+	println(sumSubarrayMins([]int{3, 1, 2, 4}))
+	println(sumSubarrayMins([]int{11, 81, 94, 43, 3}))
+}
