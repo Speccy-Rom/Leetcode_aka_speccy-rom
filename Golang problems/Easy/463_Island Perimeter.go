883a45e2165c373ac40707ba47953c0e3222e760
speccy-rom <speccy.rom@gmail.com>
2024-04-18 21:32:41 +0300

    [#463] :Island Perimeter

---
+++ Golang problems/Easy/463_Island Perimeter.go
@@ -0,0 +1,65 @@
+/*
+
+You are given row x col grid representing a map where grid[i][j] = 1 represents land and grid[i][j] = 0 represents water.
+
+Grid cells are connected horizontally/vertically (not diagonally). The grid is completely surrounded by water, and there is exactly one island (i.e., one or more connected land cells).
+
+The island doesn't have "lakes", meaning the water inside isn't connected to the water around the island. One cell is a square with side length 1. The grid is rectangular, width and height don't exceed 100. Determine the perimeter of the island.
+
+
+
+Example 1:
+
+
+Input: grid = [[0,1,0,0],[1,1,1,0],[0,1,0,0],[1,1,0,0]]
+Output: 16
+Explanation: The perimeter is the 16 yellow stripes in the image above.
+Example 2:
+
+Input: grid = [[1]]
+Output: 4
+Example 3:
+
+Input: grid = [[1,0]]
+Output: 4
+
+
+Constraints:
+
+row == grid.length
+col == grid[i].length
+1 <= row, col <= 100
+grid[i][j] is 0 or 1.
+There is exactly one island in grid.
+
+*/
+
+package main
+
+func islandPerimeter(grid [][]int) int {
+	m, n := len(grid), len(grid[0])
+	ans := 0
+	for i := 0; i < m; i++ {
+		for j := 0; j < n; j++ {
+			if grid[i][j] == 1 {
+				ans += 4
+				if i < m-1 && grid[i+1][j] == 1 {
+					ans -= 2
+				}
+				if j < n-1 && grid[i][j+1] == 1 {
+					ans -= 2
+				}
+			}
+		}
+	}
+	return ans
+}
+
+// Test cases
+func main() {
+	// Test cases
+	println(islandPerimeter([][]int{{0, 1, 0, 0}, {1, 1, 1, 0}, {0, 1, 0, 0}, {1, 1, 0, 0}})) // 16
+	println(islandPerimeter([][]int{{1}}))                                                    // 4
+	println(islandPerimeter([][]int{{1, 0}}))                                                 // 4
+
+}
