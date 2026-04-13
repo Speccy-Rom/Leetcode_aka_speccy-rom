e4e5a14f2854eb6619ddd525dca330855bf803fa
rom.spiridonov <rom.spiridonov@x5.ru>
2026-04-13 23:45:07 +0300

    add solution for problem 1848

---
+++ Golang problems/Easy/1848_Minimum Distance to the Target Element.go
@@ -0,0 +1,22 @@
+package main
+
+func getMinDistance(nums []int, target int, start int) int {
+	ans := 1 << 30
+	for i, x := range nums {
+		if t := abs(i - start); x == target && t < ans {
+			ans = t
+		}
+	}
+	return ans
+}
+
+func abs(x int) int {
+	if x < 0 {
+		return -x
+	}
+	return x
+}
+
+func main() {
+	getMinDistance([]int{1, 2, 3, 4, 5}, 5, 3)
+}
