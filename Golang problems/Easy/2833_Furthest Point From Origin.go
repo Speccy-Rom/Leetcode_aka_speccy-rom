620de5d63d141e719d0a5a0d2bdb29267053b57e
rom.spiridonov <rom.spiridonov@x5.ru>
2026-04-24 21:09:15 +0300

    add solution for problem 2833

---
+++ Golang problems/Easy/2833_Furthest Point From Origin.go
@@ -0,0 +1,19 @@
+package main
+
+import "strings"
+
+func furthestDistanceFromOrigin(moves string) int {
+	count := func(c string) int { return strings.Count(moves, c) }
+	return abs(count("L")-count("R")) + count("_")
+}
+
+func abs(x int) int {
+	if x < 0 {
+		return -x
+	}
+	return x
+}
+
+func main() {
+	furthestDistanceFromOrigin("L_RL__R")
+}
